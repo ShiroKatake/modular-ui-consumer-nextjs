6e1b789561ab9c5c06cbb6ddce36e77c383c73b0
ShiroKatake <tgkhang262@gmail.com>
2023-01-30 13:08:36 +1100

    feat: added global styles for fm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body style={{ backgroundColor: "black" }}>
+      <body style={{ backgroundColor: "black", margin: 0 }}>
         <Main />
         <NextScript />
       </body>
--- pages/fm/components.tsx
+++ pages/fm/components.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { FmButton } from "@/components/FM/Button/FmButton";
 import { IoMdPlay } from "react-icons/io";
+import { FmNavMenu } from "@/components/FM/NavMenu/FmNavMenu";
 
 const NbComponents = () => {
   return (
-    <div>
+    <div style={{
+      fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+    }}>
+      <FmNavMenu />
       <FmButton><IoMdPlay />Watch Episode 1</FmButton>
       <FmButton primary={false}>Take episode 1 quiz</FmButton>
     </div>
